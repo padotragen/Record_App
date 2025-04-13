d3ee565b7d154ae326b0388b359d1ea73649a65a
William Wright <wrighwj@gmail.com>
2025-04-13 15:06:27 -0500

    Adding Settings Page

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,15 @@ import discogs_client
 app = Flask(__name__)
 app.config.from_object(Config)
 
-#Retrieving the API KEY from the Environment and Generate the DEFAULT_FILE name
-USER_TOKEN = app.config['API_KEY']
+# Retrieving the API KEY from the Environment and Generate the DEFAULT_FILE name
+USER_TOKEN = app.config["API_KEY"]
 
 if not USER_TOKEN:
     raise ValueError("API Key is missing. Set the 'API_KEY' environment variable.")
 
-d = discogs_client.Client('ExampleApplication/0.1', user_token=USER_TOKEN)
-me=d.identity()
-app.config['DEFAULT_FILE'] = f"{me.id}.json"
+d = discogs_client.Client("ExampleApplication/0.1", user_token=USER_TOKEN)
+me = d.identity()
+app.config["DEFAULT_FILE"] = f"{me.id}.json"
 
+from app import dpmodule
 from app import routes
